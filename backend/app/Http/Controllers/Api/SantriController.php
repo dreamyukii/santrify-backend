0d8f9158f1b318681ed418804f0f9ba5620e9680
sekalengrengginang <49896401+sekalengrengginang@users.noreply.github.com>
2023-07-09 07:26:52 +0700

    feat: add login functionality

---
+++ backend/app/Http/Controllers/Api/SantriController.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Santri;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+
+class SantriController extends Controller
+{    
+    /**
+     * index
+     *
+     * @return void
+     */
+    public function index()
+    {
+        //get posts
+        $santris = Santri::latest()->paginate(5);
+
+        //return collection of posts as a resource
+        return new PostResource(true, 'List Data Posts', $santris);
+    }
+
+    /**
+     * store
+     *
+     * @param  mixed $request
+     * @return void
+     */
+
+    public function store(Request $request)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+
+            'nama' => 'required',
+            'gender' => 'required',
+            'status' => 'required',
+            'room' => 'required',
+            'division' => 'required',
+            'gambar'     => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+            
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //upload image
+        $image = $request->file('gambar');
+        $image->storeAs('public/posts', $image->hashName());
+
+        //create post
+        $santri = Santri::create([
+            'gambar'     => $image->hashName(),
+            'nama'      => $request->nama,
+            'gender'    => $request->gender,
+            'status'    => $request->status,
+            'room'      => $request->room,
+            'division'   => $request->division
+        ]);
+
+        //return response
+        return new PostResource(true, 'Data Post Berhasil Ditambahkan!', $santri);
+    }
+
+
+
+    /**
+     * show
+     *
+     * @param  mixed $post
+     * @return void
+     */
+    public function show(Santri $santri)
+    {
+        //return single post as a resource
+        return new PostResource(true, 'Data Post Ditemukan!', $santri);
+    }
+    
+    /**
+     * update
+     *
+     * @param  mixed $request
+     * @param  mixed $post
+     * @return void
+     */
+    public function update(Request $request, Santri $santri)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'gambar'     => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'nama' => 'required',
+            'gender' => 'required',
+            'status' => 'required',
+            'room' => 'required',
+            'division' => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //check if image is not empty
+        if ($request->hasFile('gambar')) {
+
+            //upload image
+            $image = $request->file('gambar');
+            $image->storeAs('public/posts', $image->hashName());
+
+            //delete old image
+            Storage::delete('public/posts/'.$santri->image);
+
+            //update post with new image
+            $santri->update([
+                'gambar'     => $image->hashName(),
+                'nama'      => $request->nama,
+                'gender'    => $request->gender,
+                'status'    => $request->status,
+                'room'      => $request->room,
+                'division'   => $request->division
+                
+            ]);
+
+        } else {
+
+            //update post without image
+            $santri->update([
+            
+            'nama'      => $request->nama,
+            'gender'    => $request->gender,
+            'status'    => $request->status,
+            'room'      => $request->room,
+            'division'   => $request->division
+            ]);
+        }
+
+        //return response
+        return new PostResource(true, 'Data Post Berhasil Diubah!', $santri);
+    }
+    
+    /**
+     * destroy
+     *
+     * @param  mixed $post
+     * @return void
+     */
+    public function destroy(Santri $santri)
+    {
+        //delete image
+        Storage::delete('public/posts/'.$santri->image);
+
+        //delete post
+        $santri->delete();
+
+        //return response
+        return new PostResource(true, 'Data Post Berhasil Dihapus!', null);
+    }
+}
