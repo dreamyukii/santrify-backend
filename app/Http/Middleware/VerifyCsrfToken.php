97cf317b3a68876b1277edfc80e40cfd8d95b7c3
sekalengrengginang <49896401+sekalengrengginang@users.noreply.github.com>
2023-07-24 17:40:09 +0000

    fix csrf again

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
-        'https://santrify.vercel.app'
+        'https://santrify.vercel.app/',
+        'https://santrify.vercel.app/dashboard/santri/create'
     ];
 }
