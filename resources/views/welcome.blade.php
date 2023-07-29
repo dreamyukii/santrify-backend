f8fac7cd9c9b41a3024cd9d39a4a0ad58bc14d06
sekalengrengginang <49896401+sekalengrengginang@users.noreply.github.com>
2023-07-29 19:57:12 +0000

    feat: add fun stuff

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,22 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<script type="text/javascript" src="{{ asset('/js/app.js') }}"></script>
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Santrify API</title>
+    <title>santrify-api</title>
 </head>
 
 <body>
-    <div> 
-        <img style="justify-content: center" src="https://tenor.com/id/view/blue-archive-arona-gif-26420148.gif">
+    <h1 class="text-primary">The Server is running!</h1>
+    <div>
+        <img src="https://tenor.com/id/view/hanayamata-gif-18772769.gif">
+        <img src="https://tenor.com/id/view/nichijou-amongus-amogus-gif-20634066.gif">
+    </div>
+    <div>
+        <img src="https://tenor.com/id/view/anime-nichijou-gif-15999921.gif">
+        <img src="https://tenor.com/id/view/hanayamata-gif-18772764.gif">
     </div>
 
 </body>
